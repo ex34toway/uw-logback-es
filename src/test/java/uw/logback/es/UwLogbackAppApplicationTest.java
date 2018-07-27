6c00bc386606eb113552d9c2022fc2184c20e8f5
liliang <649743135@qq.com>
2018-07-27 11:24:21 +0800

    删除无用依赖

--- src/test/java/uw/logback/es/UwLogbackAppApplicationTest.java
+++ src/test/java/uw/logback/es/UwLogbackAppApplicationTest.java
@@ -1,11 +1,8 @@
 package uw.logback.es;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * 测试日志输出
@@ -13,8 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author liliang
  * @since 2018-07-25
  */
-@SpringBootTest(classes = UwLogbackAppApplication.class)
-@RunWith(SpringRunner.class)
 public class UwLogbackAppApplicationTest {
 
     private static final Logger logger = LoggerFactory.getLogger(UwLogbackAppApplicationTest.class);
